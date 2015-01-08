29a9ba35c9e56b06ea576431fa1b553e579262d8
Johan Vervloet <johanv@johanv.org>
2015-01-08 20:46:24 +0100

    Refs #28, #37, #38 - first automated test.

---
+++ Chiro.CiviCrm.Wcf.Test/ContactTest.cs
@@ -0,0 +1,44 @@
+﻿/*
+   Copyright 2014 Johan Vervloet
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+using System.Linq;
+using Chiro.CiviCrm.Api.DataContracts.Requests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chiro.CiviCrm.Wcf.Test
+{
+    [TestClass]
+    public class ContactTest
+    {
+        /// <summary>
+        /// Try to retrieve contact 1, which is the default organization.
+        /// </summary>
+        [TestMethod]
+        public void Contact1Get()
+        {
+            using (var client = TestHelper.ClientGet())
+            {
+                // Get the contact, and chain the contact's addresses.
+                var resultValue = client.ContactGet(TestHelper.ApiKey, TestHelper.SiteKey, new IdRequest(1));
+
+                Assert.AreEqual(0, resultValue.IsError);
+                Assert.AreEqual(1, resultValue.Count);
+                Assert.AreEqual(1, resultValue.Id);
+                Assert.AreEqual(1, resultValue.Values.First().Id);
+            }
+        }
+    }
+}
---
+++ Chiro.CiviCrm.Wcf.Test/TestHelper.cs
@@ -0,0 +1,45 @@
+﻿/*
+   Copyright 2015 Johan Vervloet
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+using System.ServiceModel;
+using Chiro.CiviCrm.Api;
+using Chiro.CiviCrm.Wcf.Test.Properties;
+
+namespace Chiro.CiviCrm.Wcf.Test
+{
+    public static class TestHelper
+    {
+        // It is probably not recommended to keep one channel factory around for a long time.
+        // But I guess it's OK for the unit tests.
+        
+        private static readonly ChannelFactory<ICiviCrmApi> Factory = new ChannelFactory<ICiviCrmApi>("*");
+
+        public static string ApiKey
+        {
+            get { return Settings.Default.ApiKey; }
+        }
+
+        public static string SiteKey
+        {
+            get { return Settings.Default.SiteKey; }
+        }
+
+        public static ICiviCrmApi ClientGet()
+        {
+            return Factory.CreateChannel();
+        }
+    }
+}
