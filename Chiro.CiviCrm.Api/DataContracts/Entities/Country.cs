2db8f67c4d69797879fca3ebe97d287b41843b29
Johan Vervloet <johanv@johanv.org>
2015-08-04 12:16:15 +0200

    Closes #79 - support for the country api.

---
+++ Chiro.CiviCrm.Api/DataContracts/Entities/Country.cs
@@ -0,0 +1,38 @@
+﻿/*
+   Copyright 2014, 2015 Chirojeugd-Vlaanderen vzw
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+using System.Runtime.Serialization;
+using Chiro.CiviCrm.Api.Converters;
+using Newtonsoft.Json;
+
+namespace Chiro.CiviCrm.Api.DataContracts.Entities
+{
+    [DataContract]
+    public class Country
+    {
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+        [DataMember(Name = "iso_code")]
+        public string IsoCode { get; set; }
+        [DataMember(Name = "region_id")]
+        public int RegionId { get; set; }
+        [DataMember(Name = "is_province_abbreviated"), JsonProperty]
+        [JsonConverter(typeof(BoolConverter))]
+        public bool IsProvinceAbbreviated { get; set; }
+    }
+}
---
+++ Chiro.CiviCrm.Api/DataContracts/Requests/CountryRequest.cs
@@ -0,0 +1,42 @@
+﻿/*
+   Copyright 2014, 2015 Chirojeugd-Vlaanderen vzw
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+using Newtonsoft.Json;
+
+namespace Chiro.CiviCrm.Api.DataContracts.Requests
+{
+    public class CountryRequest: BaseRequest
+    {
+        [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
+        public int? Id { get; set; }
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
+        [JsonProperty("iso_code", NullValueHandling = NullValueHandling.Ignore)]
+        public string IsoCode { get; set; }
+        [JsonProperty("region_id", NullValueHandling = NullValueHandling.Ignore)]
+        public int? RegionId { get; set; }
+        [JsonProperty("is_province_abbreviated", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? IsProvinceAbbreviated { get; set; }
+
+        /// <summary>
+        /// The entity type this request is referring to.
+        /// </summary>
+        public override CiviEntity EntityType
+        {
+            get { return CiviEntity.Country; }
+        }
+    }
+}
